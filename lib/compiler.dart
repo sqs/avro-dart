d5caacd8f4b257a85401b413a1da205c4d32d255
Quinn Slack <qslack@qslack.com>
2012-10-31 03:26:52 -0700

    Stub avro_compiler library source and test

---
+++ lib/compiler.dart
@@ -0,0 +1,1 @@
+library avro_compiler;
---
+++ test/compiler_test.dart
@@ -0,0 +1,9 @@
+library avro_compiler_test;
+
+import 'package:unittest/vm_config.dart';
+import 'package:unittest/unittest.dart';
+import '../lib/compiler.dart';
+
+main() {
+  useVmConfiguration();
+}
